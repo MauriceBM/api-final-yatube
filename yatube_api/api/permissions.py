51d496611f8cdf2f4061b2a6c32424471538ed5a
Maurice <mauricelandryballamessi@gmail.com>
2026-06-14 22:30:52 +0300

    Remove duplicate permissions.py and fix anonymous read access

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,11 +1,17 @@
-# api/permissions.py
-from rest_framework import permissions
+# yatube_api/api/permissions.py
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
-class IsAuthorOrReadOnly(permissions.BasePermission):
-
+class IsAuthorOrReadOnly(IsAuthenticatedOrReadOnly):
+    """
+    Разрешает чтение всем пользователям (аутентифицированным и нет).
+    Запись/изменение/удаление доступно только автору объекта.
+    """
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        if request.method in ('GET', 'HEAD', 'OPTIONS'):
             return True
+
+        if not request.user.is_authenticated:
+            return False
 
         return obj.author == request.user
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,20 +1,27 @@
 # yatube_api/api/views.py
 from rest_framework import viewsets, filters, mixins, permissions
 from django_filters.rest_framework import DjangoFilterBackend
-from .permissions import IsAuthorOrReadOnly
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.exceptions import PermissionDenied
 
 from posts.models import Post, Comment, Group
 from .serializers import (
     PostSerializer, CommentSerializer,
     GroupSerializer, FollowSerializer
 )
+from .permissions import IsAuthorOrReadOnly
 
 
-class PostPagination(LimitOffsetPagination):
-    default_limit = 10
+class ConditionalLimitOffsetPagination(LimitOffsetPagination):
+    """
+    Пагинация с поддержкой limit/offset, активная при наличии параметров.
+    """
+    default_limit = None
     max_limit = 100
+
+    def paginate_queryset(self, queryset, request, view=None):
+        if 'limit' in request.query_params or 'offset' in request.query_params:
+            return super().paginate_queryset(queryset, request, view)
+        return None
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -24,11 +31,9 @@ class PostViewSet(viewsets.ModelViewSet):
     filter_backends = (DjangoFilterBackend, filters.SearchFilter)
     filterset_fields = ('group',)
     search_fields = ('text',)
-    pagination_class = PostPagination
+    pagination_class = ConditionalLimitOffsetPagination
 
     def perform_create(self, serializer):
-        if not self.request.user.is_authenticated:
-            raise PermissionDenied("Authentication required to create posts.")
         serializer.save(author=self.request.user)
 
 
@@ -40,9 +45,6 @@ class CommentViewSet(viewsets.ModelViewSet):
         return Comment.objects.filter(post_id=self.kwargs.get('post_id'))
 
     def perform_create(self, serializer):
-        if not self.request.user.is_authenticated:
-            raise PermissionDenied(
-                "Authentication required to create comments.")
         post = Post.objects.get(id=self.kwargs.get('post_id'))
         serializer.save(author=self.request.user, post=post)
 
@@ -63,21 +65,5 @@ class FollowViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
     def get_queryset(self):
         return self.request.user.follower.all()
 
-    def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
-
-    def create(self, request, *args, **kwargs):
-        return super().create(request, *args, **kwargs)
-
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
-
-class ConditionalLimitOffsetPagination(LimitOffsetPagination):
-    default_limit = None
-    max_limit = 100
-
-    def paginate_queryset(self, queryset, request, view=None):
-        if 'limit' in request.query_params or 'offset' in request.query_params:
-            return super().paginate_queryset(queryset, request, view)
-        return None
